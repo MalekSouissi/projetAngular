5cde1d807b77a26adc18db970667d73b04c78416
Malèk Souissi <maleksouissi751@gmail.com>
2021-01-17 22:54:17 +0100

    data

--- src/app/users/users.component.ts
+++ src/app/users/users.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit,Output,EventEmitter } from '@angular/core';
+import { Component, OnInit,Output,EventEmitter,Input } from '@angular/core';
 
 @Component({
   selector: 'app-users',
@@ -6,11 +6,13 @@ import { Component, OnInit,Output,EventEmitter } from '@angular/core';
   styleUrls: ['./users.component.css']
 })
 export class UsersComponent implements OnInit {
-  
-  users= [1 , 4 , 7 ]
+  user = {Nom:"", Prenom:"", enfant:0};
+  user0 = {Nom:"Souissi", Prenom:"malek", enfant:2};
+  user1 = {Nom:"Dhrif", Prenom:"hedil", enfant:2};
+  user2 = {Nom:"dimassi", Prenom:"Mehdi", enfant:2};
+  users= [this.user0,this.user1,this.user2 ]
   opened=false
   @Output() change: EventEmitter<boolean> = new EventEmitter();
-  ;
   constructor(
   
   ) {
@@ -22,7 +24,7 @@ export class UsersComponent implements OnInit {
 
   plusfun()
   {
-    this.users.push();
+    this.users.push(this.user);
     console.log(this.users);
   }
   toggleSidebar(){
