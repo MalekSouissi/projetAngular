242681e6fd957c3c754742f391205598d8c02afa
Malèk Souissi <maleksouissi751@gmail.com>
2021-01-17 17:17:46 +0100

    first commit

---
+++ src/app/app.module.ts
@@ -0,0 +1,27 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { ButtonComponent } from './button/button.component';
+import{SidebarModule} from 'ng-sidebar';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { UsersComponent } from './users/users.component';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    ButtonComponent,
+    UsersComponent,
+    
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule,
+    BrowserAnimationsModule,
+    SidebarModule.forRoot()
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
---
+++ src/app/users/users.component.ts
@@ -0,0 +1,24 @@
+import { Component, OnInit,Output,EventEmitter } from '@angular/core';
+@Component({
+  selector: 'app-users',
+  templateUrl: './users.component.html',
+  styleUrls: ['./users.component.css']
+})
+export class UsersComponent implements OnInit {
+  users= [1 , 4 , 7 ]
+  opened=false
+  @Output() change: EventEmitter<boolean> = new EventEmitter();
+  ;
+
+  ngOnInit(): void {
+  }
+  plusfun()
+  {
+    this.users.push(1);
+    console.log(this.users);
+  }
+  toggleSidebar(){
+    this.opened=!this.opened;
+    this.change.emit(this.opened);
+  }
+}
